975ad1480c6c88d0335916cf89f6fec280d212a9
Robin Millette <robin@millette.info>
2018-10-13 15:10:07 -0400

    Cli accepts 2nd argument, playlist index starting at 0.

--- cli.js
+++ cli.js
@@ -3,12 +3,6 @@
 // self
 const cli = require('./lib/cli')
 
-const s1 = process.argv[2]
-
-if (s1 && !s1.indexOf('https://ici.radio-canada.ca/premiere/')) {
-  cli(s1)
-    .then(console.log)
-    .catch(console.error)
-} else {
-  console.error('Required: radio-canada url.')
-}
+cli(process.argv.slice(2))
+  .then(console.log)
+  .catch(console.error)
