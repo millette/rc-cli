399704e82803e692bb6748e0b442ea7296fff222
Robin Millette <robin@millette.info>
2018-10-14 18:03:22 -0400

    New unit test (prompts.inject()).

--- test.js
+++ test.js
@@ -6,10 +6,10 @@ import test from 'ava'
 
 // self
 import { readStream, findMeta, findStream } from '.'
-import cli, { makePicks, parseAnswers } from './lib/cli'
+import cli from './lib/cli'
 
 if (!process.env.TRAVIS) {
-  test('cli', async (t) => {
+  test.serial('cli (1st)', async (t) => {
     const [one] = await cli(['https://ici.radio-canada.ca/premiere/emissions/midi-info/segments/entrevue/89953/agriculture-changements-climatiques-production-effets', '0'], true)
     t.is(one.outFilename, 'tests/7969196-657.aac')
     const { size } = statSync(one.outFilename)
@@ -20,24 +20,21 @@ if (!process.env.TRAVIS) {
     unlinkSync(one.outFilename)
     unlinkSync(jsonfile)
   })
-}
 
-test('cli makePick', (t) => {
-  const [{ type, name, message }] = makePicks([{ Title: 'El Title', Duration: 120 }])
-  t.is(type, 'confirm')
-  t.is(name, 'confirm.0')
-  t.is(message, 'Download El Title (env. 2m.)')
-})
-
-test('cli parseAnswers', (t) => {
-  const gg = parseAnswers({
-    confirm: {
-      'fee': false,
-      'fo': true
-    }
+  test.only('cli (inject)', async (t) => {
+    const all = await cli(['https://ici.radio-canada.ca/premiere/emissions/midi-info/segments/entrevue/89953/agriculture-changements-climatiques-production-effets'], true)
+    const [one] = all
+    console.log('ALLLL:', all)
+    t.is(one.outFilename, 'tests/7969196-657.aac')
+    const { size } = statSync(one.outFilename)
+    t.is(size, 12263336)
+    const jsonfile = one.outFilename.replace('.aac', '.json')
+    const json = require(`./${jsonfile}`)
+    t.is(json.Broad, one.Broad)
+    // unlinkSync(one.outFilename)
+    // unlinkSync(jsonfile)
   })
-  t.is(gg[0], 'fo')
-})
+}
 
 test('FindStream (3 segments)', async (t) => {
   const all = await findMeta('https://ici.radio-canada.ca/premiere/premiereplus/science/p/49306/limprimante-3d-arrive-et-elle-va-changer-nos')
@@ -55,7 +52,6 @@ test('FindStream (3 segments)', async (t) => {
 })
 
 test('Response code 500 (Internal Server Error)', (t) => t.throwsAsync(findMeta('https://ici.radio-canada.ca/premiere/emissions/midi-info/segments/entrevue/989953/agriculture-changements-climatiques-production-effets'), 'Response code 500 (Internal Server Error)'))
-
 test('No valid items match #1', (t) => t.throwsAsync(findMeta('http://ici.radio-canada.ca/premiere/emissions/midi-info/segments/entrevue/9953/agriculture-changements-climatiques-production-effets'), 'No valid items match.'))
 test('No valid items match #2', (t) => t.throwsAsync(findMeta('ici.radio-canada.ca/premiere/emissions/midi-info/segments/entrevue/9953/agriculture-changements-climatiques-production-effets'), 'No valid items match.'))
 test('No valid items match #3', (t) => t.throwsAsync(findMeta('https://ici.radio-canada.ca/premiere/premiereplus'), 'No items found.'))
