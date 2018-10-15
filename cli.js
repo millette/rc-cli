4ced73ab0ee611df4b0c02544688a00401498601
Robin Millette <robin@millette.info>
2018-10-14 21:58:21 -0400

    Help text.

--- cli.js
+++ cli.js
@@ -5,18 +5,43 @@
 // self
 const cli = require('./lib/cli')
 
-// simulate meow for now
-const poc = () => {
-  const [input, id] = process.argv.slice(2)
-  return {
-    input: [input],
-    flags: {
-      id,
-      dir: 'outs'
+// npm
+const meow = require('meow')
+
+const args = meow(`
+  Usage
+
+    $ radcan URL
+
+  Options
+    --id      -i  ID (1, 2, 3...)
+    --dir     -d  Output directory, defaults to current
+    --all     -a  Download all segments
+    --quiet   -q  Quiet mode
+    --version     Version
+  `, {
+  inferType: true,
+  flags: {
+    id: {
+      type: 'string',
+      alias: 'i'
+    },
+    dir: {
+      type: 'string',
+      alias: 'd',
+      default: '.'
+    },
+    all: {
+      type: 'boolean',
+      alias: 'a'
+    },
+    quiet: {
+      type: 'boolean',
+      alias: 'q'
     }
   }
-}
+})
 
-cli(poc())
-  .then(console.log)
+cli(args)
+  .then(() => console.log('All good.'))
   .catch(console.error)
